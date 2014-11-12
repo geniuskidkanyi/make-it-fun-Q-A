a5667420e06a0c588c0f5a93d407430067207bba
muhammed kanyi <geniuskid@g3gambia.com>
2014-11-12 14:17:36 +0000

    made the user registration module,controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
       @user = User.new
   end
+
+    def create
+    @user = User.new(user_params) ## Invoke user_params method
+    if @user.save
+       redirect_to root_path @user, notice: 'Author was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+    private
+  ## Strong Parameters
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+def user_params
+    params.require(:user).permit(:username, :password_digest, :password, :password_confirmation)
+  end
+has_secure_password
 
-    validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {in: 3..15}
-    validates :password, length: {in: 4..15}
-    validates :password_confirmation, length: {in: 4..15}
+  validates :username, presence: true
+	validates :password, length: { in: 4..8 }
+	validates :password_confirmation, length: { in: 4..8 }
+
+#	def your_questions(params)
+#		questions.paginate(page: params[:page], order: 'created_at DESC', per_page: 3)
+#	end
 end
